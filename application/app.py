ff35cbc9d89ea0b5b18a07a5053fadc40b8abee0
HYPERPC <nerocross507@gmail.com>
2022-08-09 19:02:56 +0300

    rebuilding code

---
+++ application/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restx import Api
+
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    db.init_app(app)
+
+    with app.app_context():
+
+        api = Api(app, version='1.0', description='Movies API', prefix='/api')
+        app.config['api'] = api
+
+        from application import routes
+
+        return app
---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+
+class Config:
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.getcwd(), 'test.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
